f1f135d64253c326ff5ce85152e0fa230f1ef4dd
JOM <jom@hrpsolutions>
2024-11-05 12:06:25 +0300

    changeing the footer description

--- src/Components/Layouts/HorizontalLayout/components/Footer.tsx
+++ src/Components/Layouts/HorizontalLayout/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Container, Row, Col } from "reactstrap"
+import { COMPANY_NAME } from "../../../../constants/app.constants"
+
 
 const Footer = () => {
     return (
@@ -7,10 +9,10 @@ const Footer = () => {
             <footer className="footer">
                 <Container fluid={true}>
                     <Row>
-                        <Col md={6}>{new Date().getFullYear()} © Ehub.</Col>
+                        <Col md={6}>{new Date().getFullYear()} © Ehub.{COMPANY_NAME}</Col>
                         <Col md={6}>
                             <div className="text-sm-end d-none d-sm-block">
-                                UDB
+                                {COMPANY_NAME}
                             </div>
                         </Col>
                     </Row>
