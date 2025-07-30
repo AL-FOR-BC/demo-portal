3004fb3fa42a54d1a09c919dc67c1727d32e729e
CODE-WITH-JOM <mukiibijoseph19@gmail.com>
2025-07-30 16:02:21 +0300

    deployment of demo

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ function App() {
   return (
     <Provider store={store}>
       <MsalProvider instance={pca}>
-        <BrowserRouter basename="/rom">
+        <BrowserRouter basename="/">
           <IdleTimerContainer />
           <ToastContainer theme="colored" />
           <Layout />
